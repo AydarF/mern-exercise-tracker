5d8e93b427d15f2d284ad489ae3e9580e89341b2
AydarF <aydar_fayzullin@yahoo.com>
2019-12-14 14:21:26 -0500

    Add fetch & display exercises functionality

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class ExercisesList extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            exercises: [] 
+        };
+
+        this.deleteExercise = this.deleteExercise.bind(this);
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:8080/exercises/')
+            .then(response => {
+                this.setState({
+                    exercises: response.data
+                })
+            }).catch(error => {
+                console.log('Error: ', error);
+            });
+    }
+
     render() {
         return (
             <div>
