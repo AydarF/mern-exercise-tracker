31f08db70106b066c68c3c2a77f6286239335f35
AydarF <aydar_fayzullin@yahoo.com>
2020-02-07 14:08:20 -0500

    Refactor server.js file for production

--- backend/server.js
+++ backend/server.js
@@ -36,14 +36,11 @@ app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
 //Serve static assets if in production
-if(process.env.NODE_ENV === 'production'){
-    // Set static folder
-    app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, "../build")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "../build/index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
