90d16b01a8bf7d762fe2eeea7f05efcf83ea17b8
AydarF <aydar_fayzullin@yahoo.com>
2020-02-07 13:10:17 -0500

    Prepare & deploy app to Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
@@ -33,6 +34,16 @@ connection.once('open', () => {
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
+
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
